28961feb4434194422921ce5d1fd07ee2ba83548
Nikita Obergan <Obergannikita@gmail.com>
2023-09-11 12:14:36 -0400

    add dummy data

--- src/Components/PostList/PostList.jsx
+++ src/Components/PostList/PostList.jsx
@@ -1,9 +1,22 @@
 import Post from "../Postt/Post";
 
-const PostList = ({ posts }) => {
-  const postItem = posts.map((post) => <Post postObj={post} />);
+const PostList = ({}) => {
+  //   const postItem = posts.map((post) => <Post postObj={post} />);
 
-  return <div>{postItem}</div>;
+  //   return <div>{postItem}</div>;
+
+  //   Creating dummy data
+  return (
+    <div>
+      <div>David Lagrange</div>
+      <p>
+        I recently wen tinto the woods to search for the oldest tree I could
+        find. To my luck I found a very old hemlock around 200-250 years old.
+        Let me know if you would like me to show it to you!
+      </p>
+      <button>Like</button> <button>Dislike</button>
+    </div>
+  );
 };
 
 export default PostList;
